3b277030df754ea13d7a076b88d1a9b9767ae6cf
hailong16122000 <78597681+hailong16122000@users.noreply.github.com>
2021-02-26 21:10:27 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './src/screens/Login';
+import Product from './src/screens/Product';
+
+import RootNativation from './src/Navigations';
+
+export default function App() {
+  return (
+    <RootNativation></RootNativation>
+  );
+}
